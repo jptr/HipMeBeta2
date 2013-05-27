a230281af51c87902b6a25a809462ffec8c14aec
SimonFrr <sim.frr@gmail.com>
2013-05-27 15:48:46 +0200

    architecture listen section

--- hipme/hipme/hip_engine/views.py
+++ hipme/hipme/hip_engine/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 from django.forms.models import inlineformset_factory, modelformset_factory
-from hip_engine.forms import ProfileEmailNotificationForm, UserEmailForm, TracklistForm, TrackForm
+# from hip_engine.forms import ProfileEmailNotificationForm, UserEmailForm, TracklistForm, TrackForm
 
 from hip_engine.models import User, UserProfile, Track, Bundle, Tracklist
 
@@ -18,16 +18,16 @@ def login_form(request):
         return test(request)
 
 def test(request):
-    return render_to_response('hip_engine/base.html', context_instance=RequestContext(request))
+    return render_to_response('hip_engine/listen22_network_music_popular.html', context_instance=RequestContext(request))
 
-@login_required
-def test_forms(request):
-    email_form = UserEmailForm(instance=request.user)
-    email_notif_form = ProfileEmailNotificationForm(instance=request.user)
+# @login_required
+# def test_forms(request):
+#     email_form = UserEmailForm(instance=request.user)
+#     email_notif_form = ProfileEmailNotificationForm(instance=request.user)
 
-    tracklist_form = TracklistForm(instance=request.user, username=request.user.username)
+#     tracklist_form = TracklistForm(instance=request.user, username=request.user.username)
 
-    track_form = TrackForm()
-    # tracklist_formset = TracklistFormset(initial=[{'owner': request.user.get_profile(), 'bundlego':bundlego}])
+#     track_form = TrackForm()
+#     # tracklist_formset = TracklistFormset(initial=[{'owner': request.user.get_profile(), 'bundlego':bundlego}])
 
-    return render_to_response('hip_engine/forms.html', {'email_form':email_form, 'email_notif_form':email_notif_form, 'tracklist_form':tracklist_form, 'track_form':track_form}, context_instance=RequestContext(request))
+#     return render_to_response('hip_engine/forms.html', {'email_form':email_form, 'email_notif_form':email_notif_form, 'tracklist_form':tracklist_form, 'track_form':track_form}, context_instance=RequestContext(request))
