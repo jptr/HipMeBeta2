c6097afb9a5d1aa288e5a2d404c84d951987131d
SimonFrr <sim.frr@gmail.com>
2013-06-04 18:23:49 +0200

    db_filler

---
+++ hipme/database/hipme_db_setup_3.py
@@ -0,0 +1,442 @@
+INSTANCES = [
+
+{
+    "model":"UserProfile",
+    "username":"ChristineBoutin",
+    "email":"a1@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"FrancoisMitterrand",
+    "email":"a2@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"JesusCHRIST",
+    "email":"a3@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"FleurPellerin",
+    "email":"a4@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"Philippe-Mustar",
+    "email":"a5@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"NicolasAnelka",
+    "email":"a6@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"Obi-Wan_Kenobi",
+    "email":"a7@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"MarcDutroux",
+    "email":"a8@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"Buddha",
+    "email":"a9@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"Tom_Cruise",
+    "email":"a10@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"LexLuthor",
+    "email":"a11@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"UserProfile",
+    "username":"MichaelJordan",
+    "email":"a12@mail.com",
+    "password":"database",
+},
+
+{
+    "model":"TrackList",
+    "title":"Summer Kiff",
+    "description":"coucou les potes",
+    "tags":
+        [
+            "rock",
+            "rap",
+            "summer-house",
+        ],
+    "tracks":
+        [
+            {
+                "artist":"The White Stripes",
+                "name":"Little bird",
+                "url":"http://www.youtube.com/watch?v=syDgyjECYZA",
+            },
+            {
+                "artist":"Flume",
+                "name":"On Top feat. T.Shirt",
+                "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+            },
+        ],
+    "bundlebacks":
+        [
+            {
+            "owner":"ChristineBoutin",
+            "tracks":
+                [
+                    {
+                        "artist":"Flume",
+                        "name":"On Top feat. T.Shirt",
+                        "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+                    },
+                    {
+                        "artist":"Woodkid",
+                        "name":"Wasteland (SonnSee Remix/Edit)",
+                        "url":"https://soundcloud.com/sonnsee/woodkid-wasteland-sonnsee/",
+                    },
+                    {
+                        "artist":"CasioKids",
+                        "name":"Verdens Største Land",
+                        "url":"http://www.youtube.com/watch?v=jz0pEUI9CtA",
+                    },
+                ],
+            },
+            {
+            "owner":"Tom_Cruise",
+            "tracks":
+                [
+                    {
+                        "artist":"Tom Jones",
+                        "name":"Help Yourself",
+                        "url":"http://www.youtube.com/watch?v=OvHkWl8HfrI",
+                    },
+                    {
+                        "artist":"De La Soul & Lucien Papalu",
+                        "name":"Say La Vee",
+                        "url":"http://www.youtube.com/watch?v=Mye72SqtIzg",
+                    },
+                ],
+            },
+            {
+            "owner":"JesusCHRIST",
+            "tracks":
+                [
+                    {
+                        "artist":"Flume",
+                        "name":"On Top feat. T.Shirt",
+                        "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+                    },
+                    {
+                        "artist":"Woodkid",
+                        "name":"Wasteland (SonnSee Remix/Edit)",
+                        "url":"https://soundcloud.com/sonnsee/woodkid-wasteland-sonnsee/",
+                    },
+                    {
+                        "artist":"Smokey Joe & The Kid feat Youthstar",
+                        "name":"Smokey Joe's Café",
+                        "url":"https://www.youtube.com/watch?v=oEZP5Y8HQI0",
+                    },
+                ],
+            },
+        ],
+    "owner":"FrancoisMitterrand",
+    "is_finished":False,
+},
+
+{
+    "model":"TrackList",
+    "title":"Happy People in da place with a very long title that is going to break the front-end",
+    "description":"Kiss my CSS",
+    "tags":
+        [
+            "funk",
+            "soul",
+            "hipppity-hop",
+        ],
+    "tracks":
+        [
+            {
+                "artist":"The White Stripes",
+                "name":"Little bird",
+                "url":"http://www.youtube.com/watch?v=syDgyjECYZA",
+            },
+            {
+                "artist":"Flume",
+                "name":"On Top feat. T.Shirt",
+                "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+            },
+            {
+                "artist":"CasioKids",
+                "name":"Verdens Største Land",
+                "url":"http://www.youtube.com/watch?v=jz0pEUI9CtA",
+            },
+        ],
+    "bundlebacks":
+        [
+            {
+            "owner":"Buddha",
+            "tracks":
+                [
+                    {
+                        "artist":"Flume",
+                        "name":"On Top feat. T.Shirt",
+                        "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+                    },
+                ],
+            },
+            {
+            "owner":"Tom_Cruise",
+            "tracks":
+                [
+                    {
+                        "artist":"Tom Jones",
+                        "name":"Help Yourself",
+                        "url":"http://www.youtube.com/watch?v=OvHkWl8HfrI",
+                    },
+                    {
+                        "artist":"De La Soul & Lucien Papalu",
+                        "name":"Say La Vee",
+                        "url":"http://www.youtube.com/watch?v=Mye72SqtIzg",
+                    },
+                ],
+            },
+            {
+            "owner":"JesusCHRIST",
+            "tracks":
+                [
+                    {
+                        "artist":"Darius & Crayon",
+                        "name":"Alicia",
+                        "url":"https://soundcloud.com/dariusofficial/alicia/",
+                    },
+                    {
+                        "artist":"Woodkid",
+                        "name":"Wasteland (SonnSee Remix/Edit)",
+                        "url":"https://soundcloud.com/sonnsee/woodkid-wasteland-sonnsee/",
+                    },
+                    {
+                        "artist":"BlakRoc",
+                        "name":"Hard Times (ft. NOE)",
+                        "url":"http://www.youtube.com/watch?v=VY-T_-Az_z4",
+                    },
+                    {
+                        "artist":"Bixel Boys",
+                        "name":"Love Like This (Viceroy Remix)",
+                        "url":"https://soundcloud.com/viceroymusic/bixel-boys-love-like-this-viceroy-remix/",
+                    },
+                    {
+                        "artist":"Smokey Joe & The Kid feat Youthstar",
+                        "name":"Smokey Joe's Café",
+                        "url":"https://www.youtube.com/watch?v=oEZP5Y8HQI0",
+                    },
+                ],
+            },
+        ],
+    "owner":"Obi-Wan_Kenobi",
+    "is_finished":True,
+},
+
+{
+    "model":"TrackList",
+    "title":"Mini-mixtape for mini-people",
+    "description":"Lolcats",
+    "tags":
+        [
+        ],
+    "tracks":
+        [
+            {
+                "artist":"The White Stripes",
+                "name":"Little bird",
+                "url":"http://www.youtube.com/watch?v=syDgyjECYZA",
+            },
+            {
+                "artist":"Flume",
+                "name":"On Top feat. T.Shirt",
+                "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+            },
+            {
+                "artist":"CasioKids",
+                "name":"Verdens Største Land",
+                "url":"http://www.youtube.com/watch?v=jz0pEUI9CtA",
+            },
+        ],
+    "bundlebacks":
+        [
+        ],
+    "owner":"LexLuthor",
+    "is_finished":False,
+},
+
+{
+    "model":"TrackList",
+    "title":"CheckThisOut",
+    "description":"Best tunes ever",
+    "tags":
+        [
+            "life_soundtrack",
+        ],
+    "tracks":
+        [
+            {
+                "artist":"The White Stripes",
+                "name":"Little bird",
+                "url":"http://www.youtube.com/watch?v=syDgyjECYZA",
+            },
+            {
+                "artist":"Flume",
+                "name":"On Top feat. T.Shirt",
+                "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+            },
+            {
+                "artist":"CasioKids",
+                "name":"Verdens Største Land",
+                "url":"http://www.youtube.com/watch?v=jz0pEUI9CtA",
+            },
+            {
+                "artist":"CasioKids",
+                "name":"Verdens Største Land",
+                "url":"http://www.youtube.com/watch?v=jz0pEUI9CtA",
+            },
+            {
+                "artist":"CasioKids",
+                "name":"Verdens Største Land",
+                "url":"http://www.youtube.com/watch?v=jz0pEUI9CtA",
+            },
+        ],
+    "bundlebacks":
+        [
+        ],
+    "owner":"LexLuthor",
+    "is_finished":True,
+},
+
+{
+    "model":"TrackList",
+    "title":"Emptiness",
+    "description":"Nothing in there",
+    "tags":
+        [
+        ],
+    "tracks":
+        [
+        ],
+    "bundlebacks":
+        [
+        ],
+    "owner":"MichaelJordan",
+    "is_finished":False,
+},
+
+{
+    "model":"TrackList",
+    "title":"Wow, great music in there",
+    "description":"Give me some more",
+    "tags":
+        [
+            "pleasure",
+            "listening",
+            "loveit",
+            "wowIlovetags",
+            "soAwesome",
+            "overloadthedb",
+            "this_is_getting_annoying",
+        ],
+    "tracks":
+        [
+        ],
+    "bundlebacks":
+        [
+            {
+            "owner":"NicolasAnelka",
+            "tracks":
+                [
+                    {
+                        "artist":"Flume",
+                        "name":"On Top feat. T.Shirt",
+                        "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+                    },
+                    {
+                        "artist":"Woodkid",
+                        "name":"Wasteland (SonnSee Remix/Edit)",
+                        "url":"https://soundcloud.com/sonnsee/woodkid-wasteland-sonnsee/",
+                    },
+                    {
+                        "artist":"CasioKids",
+                        "name":"Verdens Største Land",
+                        "url":"http://www.youtube.com/watch?v=jz0pEUI9CtA",
+                    },
+                ],
+            },
+            {
+            "owner":"Tom_Cruise",
+            "tracks":
+                [
+                    {
+                        "artist":"Tom Jones",
+                        "name":"Help Yourself",
+                        "url":"http://www.youtube.com/watch?v=OvHkWl8HfrI",
+                    },
+                    {
+                        "artist":"De La Soul & Lucien Papalu",
+                        "name":"Say La Vee",
+                        "url":"http://www.youtube.com/watch?v=Mye72SqtIzg",
+                    },
+                ],
+            },
+            {
+            "owner":"FleurPellerin",
+            "tracks":
+                [
+                    {
+                        "artist":"Flume",
+                        "name":"On Top feat. T.Shirt",
+                        "url":"http://www.youtube.com/watch?v=l-QVmsqetB8",
+                    },
+                    {
+                        "artist":"Woodkid",
+                        "name":"Wasteland (SonnSee Remix/Edit)",
+                        "url":"https://soundcloud.com/sonnsee/woodkid-wasteland-sonnsee/",
+                    },
+                    {
+                        "artist":"Smokey Joe & The Kid feat Youthstar",
+                        "name":"Smokey Joe's Café",
+                        "url":"https://www.youtube.com/watch?v=oEZP5Y8HQI0",
+                    },
+                ],
+            },
+        ],
+    "owner":"FrancoisMitterrand",
+    "is_finished":False,
+},
+
+]
