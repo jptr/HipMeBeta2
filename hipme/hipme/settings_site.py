3de45361f66f9eda17d53ee57189c403f4611a27
jptr <jnpatry@gmail.com>
2013-07-10 03:15:15 +0200

    settings site + url conf for static

--- hipme/hipme/settings_site.py
+++ hipme/hipme/settings_site.py
@@ -63,17 +63,16 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/hipme/hipme/hipme/public/static_media/'
+STATIC_ROOT = '/home/hipme/hipme/hipme/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/' if DEBUG else 'http://hipme.fm/static/'
+STATIC_URL = '/static/'
 
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    'C:/Patry/Dev/HipMeBeta2/hipme/hipme/static',
-    'C:/Patry/Dev/django-chosen-master/chosen/static',
+    # '/home/hipme/hipme/hipme/static/',
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
