d660fbf49be75cb0a2aceb7e8ea69f2e829cac64
SimonFrr <sim.frr@gmail.com>
2013-11-22 02:18:26 +0100

    email when 2days left to contribute

--- hipme/hipme/hip_engine/mailer.py
+++ hipme/hipme/hip_engine/mailer.py
@@ -10,7 +10,7 @@ def generate_body_new_mixtape(profile_to_mail, tracklist):
     str1 = "Hey "+ profile_to_mail.user.username + ","
     mixtape_url = 'http://hipme.fm/profile/'+profile_to_mail.user.username+'/pending/'
     str2 = tracklist.owner.user.username + " created a new mixtape called " + tracklist.title + ", and he/she asked you to contribute! Go check it out: " + mixtape_url
-    str3 = "Add tracks to that mixtape. For every track "+ tracklist.owner.user.username + " keeps in his/her mixtape, you get points!"
+    str3 = "Add tracks to this mixtape. For every track "+ tracklist.owner.user.username + " keeps in his/her mixtape, you get points!"
     str4 = "Keep da hip,"
     str5 = "The hipmasters."
     edit_url = 'http://hipme.fm/profile/'+profile_to_mail.user.username+'/edit/'
@@ -50,6 +50,23 @@ def generate_body_mixtape_to_close(user_to_mail, tracklist):
     str3 = "Pick the tracks you want to keep in this mixtape, then don't forget to close it! Contributors get points when you keep one of their tracks."
     str4 = "Keep da hip,"
     str5 = "The hipmasters."
-    edit_url = 'http://hipme.fm/profile/'+user_to_mail.user.username+'/edit/'
+    edit_url = 'http://hipme.fm/profile/'+ user_to_mail.user.username +'/edit/'
+    str6 = "Don't forget that you can always go to the settings page to edit your account data and your notification preferences: " + edit_url
+    return str1 + "\n\n" + str2 + "\n\n" + str3 + "\n\n" + str4 +"\n"+ str5 + "\n\n" + str6
+
+def generate_header_contribute(user_to_mail):
+    return "[hipme] 2 days left to contribute to " + user_to_mail + "'s mixtape!"
+
+def generate_body_contribute(user_to_mail, tracklist):
+    str1 = "Hey "+ user_to_mail.user.username + ","
+    mixtape_url = 'http://hipme.fm/profile/'+ user_to_mail.user.username +'/pending/'
+    if tracklist.title:
+        str2 = "Only 48 hours left to contribute to " + tracklist.owner.user.username + "'s mixtape called " + tracklist.title + "! Go check it out: " + mixtape_url
+    else:
+        str2 = "Only 48 hours left to contribute to " + tracklist.owner.user.username + "'s mixtape!"
+    str3 = "Add tracks to this mixtape. For every track "+ tracklist.owner.user.username + " keeps in his/her mixtape, you get points!"
+    str4 = "Keep da hip,"
+    str5 = "The hipmasters."
+    edit_url = 'http://hipme.fm/profile/'+profile_to_mail.user.username+'/edit/'
     str6 = "Don't forget that you can always go to the settings page to edit your account data and your notification preferences: " + edit_url
     return str1 + "\n\n" + str2 + "\n\n" + str3 + "\n\n" + str4 +"\n"+ str5 + "\n\n" + str6
